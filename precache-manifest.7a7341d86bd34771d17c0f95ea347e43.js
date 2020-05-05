106aec6505396bf22ca37342e394a38016ffd4de
Edwin Liléo <edwin.lileo@icloud.com>
2020-05-05 07:58:45 +0200

    Updates

---
+++ precache-manifest.7a7341d86bd34771d17c0f95ea347e43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10ace40a4c44df961aa61d8dc84cfbd",
+    "url": "/monsters-rolodex-2/index.html"
+  },
+  {
+    "revision": "8ef42c5119ffe6fc4081",
+    "url": "/monsters-rolodex-2/static/css/main.3d85012d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex-2/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-2/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef42c5119ffe6fc4081",
+    "url": "/monsters-rolodex-2/static/js/main.fb48aae7.chunk.js"
+  },
+  {
+    "revision": "5638a3c8649e651c842f",
+    "url": "/monsters-rolodex-2/static/js/runtime-main.0b5a7502.js"
+  }
+]);
